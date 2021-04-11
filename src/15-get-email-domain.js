a33a91e73186d3a132ab9f7387cee0aff57a954a
Vitaly-Khvisevich <68824186+Vitaly-Khvisevich@users.noreply.github.com>
2021-04-11 15:37:49 +0300

    feat: add 15 task

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,11 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const regex = /@((\w+-+)|(\w+\.))*\w{1,63}.[a-zA-Z]{2,6}/i;
+  let res = email.match(regex)[0];
+  res = res.split('@');
+  return (res[1]);
 }
 
 module.exports = getEmailDomain;
