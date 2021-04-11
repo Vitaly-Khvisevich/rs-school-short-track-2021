550fff04e0947ee6a062eca7cef784991fe4b91c
Vitaly-Khvisevich <68824186+Vitaly-Khvisevich@users.noreply.github.com>
2021-04-11 15:36:07 +0300

    feat: add 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const newstr = str;
+  let obj = {};
+  let strans = '';
+  for (let i = 0; i < newstr.length; i++) {
+    if (newstr[i] in obj) obj[newstr[i]]++;
+    else {
+      let temp = '';
+      Object.entries(obj).forEach((e) => { temp += `${e[1] <= 1 ? '' : e[1]}${e[0]}`; });
+      strans += `${temp}`;
+      obj = {};
+      obj[newstr[i]] = 1;
+    }
+  }
+  Object.entries(obj).forEach((e) => { strans += `${e[1] <= 1 ? '' : e[1]}${e[0]}`; });
+  return (strans);
 }
 
 module.exports = encodeLine;
