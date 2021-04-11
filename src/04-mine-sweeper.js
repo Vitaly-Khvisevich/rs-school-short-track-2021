c626dba26c20d5d0acc1e6446ee55365ebd7e30d
Vitaly-Khvisevich <68824186+Vitaly-Khvisevich@users.noreply.github.com>
2021-04-11 15:32:47 +0300

    feat: add 1 task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,53 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newArray = matrix.map((arr) => arr.slice());
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = 0; j < newArray[i].length; j++) {
+      if (newArray[i][j] === true) {
+        if (j - 1 >= 0) {
+          if (newArray[i][j - 1] === false) newArray[i][j - 1] = 1;
+          else if (Number.isInteger(newArray[i][j - 1])) newArray[i][j - 1]++;
+        }
+        if (i + 1 < newArray.length && j - 1 >= 0) {
+          if (newArray[i + 1][j - 1] === false) newArray[i + 1][j - 1] = 1;
+          else if (Number.isInteger(newArray[i + 1][j - 1])) newArray[i + 1][j - 1]++;
+        }
+        if (i + 1 < newArray.length) {
+          if (newArray[i + 1][j] === false) newArray[i + 1][j] = 1;
+          else if (Number.isInteger(newArray[i + 1][j])) newArray[i + 1][j]++;
+        }
+        if (i + 1 < newArray.length && j + 1 < newArray[i].length) {
+          if (newArray[i + 1][j + 1] === false) newArray[i + 1][j + 1] = 1;
+          else if (Number.isInteger(newArray[i + 1][j - 1])) newArray[i + 1][j + 1]++;
+        }
+        if (j + 1 < newArray[i].length) {
+          if (newArray[i][j + 1] === false) newArray[i][j + 1] = 1;
+          else if (Number.isInteger(newArray[i + 1][j - 1])) newArray[i][j + 1]++;
+        }
+        if (i - 1 >= 0 && j + 1 < newArray[i].length) {
+          if (newArray[i - 1][j + 1] === false) newArray[i - 1][j + 1] = 1;
+          else if (Number.isInteger(newArray[i + 1][j - 1])) newArray[i - 1][j + 1]++;
+        }
+        if (i - 1 >= 0) {
+          if (newArray[i - 1][j] === false) newArray[i - 1][j] = 1;
+          else if (Number.isInteger(newArray[i + 1][j - 1])) newArray[i - 1][j]++;
+        }
+        if (i - 1 >= 0 && j - 1 >= 0) {
+          if (newArray[i - 1][j - 1] === false) newArray[i - 1][j - 1] = 1;
+          else if (Number.isInteger(newArray[i - 1][j - 1])) newArray[i - 1][j - 1]++;
+        }
+      }
+    }
+  }
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = 0; j < newArray[i].length; j++) {
+      if (newArray[i][j] === true) newArray[i][j] = 1;
+      else if (newArray[i][j] === false) newArray[i][j] = 0;
+    }
+  }
+  return (newArray);
 }
 
 module.exports = minesweeper;
